208c9eb071432a2f44044df8020da808d9eada57
stevenjburns@msn.com <stevenjburns@msn.com>
2018-01-31 19:58:56 -0700

    Moved the WPF folder of this project from the original repo

---
+++ Application/Application.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Reflection;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace SJB.MeritViewer.WPF.Application
+  {
+  class ApplicationEntry
+    {
+    [STAThread]
+    public static void Main(String[] args)
+      {
+      SingleInstanceApplication singleinstanceapp = new SingleInstanceApplication();
+      singleinstanceapp.Run(args);
+      }
+    }
+
+  public class SingleInstanceApplication : WindowsFormsApplicationBase
+    {
+    private System.Windows.Application app;
+
+    public SingleInstanceApplication()
+      { this.IsSingleInstance = true; }
+    
+    protected override bool OnStartup(Microsoft.VisualBasic.ApplicationServices.StartupEventArgs eventArgs)
+      {
+      app = new System.Windows.Application();
+      WindowMain vw = new WindowMain();
+      
+      vw.Title = "MeritViewer v" + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+      app.Run(vw);
+      return false;
+      }
+    
+    protected override void OnStartupNextInstance(StartupNextInstanceEventArgs eventArgs)
+      {
+      if (app.MainWindow.WindowState == WindowState.Minimized)
+      app.MainWindow.WindowState = WindowState.Normal;
+
+      app.MainWindow.Activate();
+      eventArgs.BringToForeground = true;
+      }
+    }
+  }
